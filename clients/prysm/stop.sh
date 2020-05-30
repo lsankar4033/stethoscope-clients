dc0daf3690ce4f11e68acfc2e7795fea1e502e3c
lsankar4033 <lsankar4033@gmail.com>
2020-05-29 19:32:21 -0700

    Add prysm test + fill out prysm stop script

--- clients/prysm/stop.sh
+++ clients/prysm/stop.sh
@@ -1,1 +1,5 @@
 #!/bin/bash
+
+# TODO: use a name passed in in the future
+docker stop prysm
+docker rm prysm
--- tests/test_connect.py
+++ tests/test_connect.py
@@ -4,11 +4,11 @@ from pyrum import SubprocessConn, Rumor
 import trio
 
 from lib.runner import start_instance, stop_instance
-from lib.types import ENR, InstanceConfig
+from lib.types import InstanceConfig
 from tests.helpers import connect_rumor, TEST_ENR, BEACON_STATE_LOCATION
 
 
-async def test_connect():
+async def test_lighthouse():
     config = InstanceConfig('lighthouse', BEACON_STATE_LOCATION, TEST_ENR)
     start_instance(config)
 
@@ -17,7 +17,24 @@ async def test_connect():
             async with trio.open_nursery() as nursery:
                 rumor = Rumor(conn, nursery)
                 peer_id = await connect_rumor(rumor, TEST_ENR.enr)
-                print(f'lighthouse peer_id: {peer_id}')
+
+                # NOTE: this may not be the exact test
+                assert peer_id is not None
+                nursery.cancel_scope.cancel()
+
+    finally:
+        stop_instance(config)
+
+
+async def test_prysm():
+    config = InstanceConfig('prysm', BEACON_STATE_LOCATION, TEST_ENR)
+    start_instance(config)
+
+    try:
+        async with SubprocessConn(cmd='rumor bare') as conn:
+            async with trio.open_nursery() as nursery:
+                rumor = Rumor(conn, nursery)
+                peer_id = await connect_rumor(rumor, TEST_ENR.enr)
 
                 # NOTE: this may not be the exact test
                 assert peer_id is not None
