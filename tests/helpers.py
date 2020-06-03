739be78c3824e38c027aa1e8d314890dbbb9ee5a
lsankar4033 <lsankar4033@gmail.com>
2020-06-03 08:32:00 -0700

    WIP

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,6 +1,6 @@
 from lib.types import ENR
 
-BEACON_STATE_LOCATION = 'ssz/tests.ssz'
+BEACON_STATE_LOCATION = 'ssz/tests_mainnet.ssz'
 
 # NOTE: currently identical to the ENR in single_client_genesis suite, but maybe not this in the future
 TEST_ENR_DICT = {
@@ -16,17 +16,18 @@ TEST_ENR_DICT = {
 TEST_ENR = ENR(**TEST_ENR_DICT)
 
 
-def write_beacon_state():
+def write_beacon_state(config='minimal'):
     """
     This method outlines how we build beacon state for tests.
     """
     from eth2spec.config.config_util import prepare_config
-    prepare_config('tests/config', 'minimal')
+    prepare_config('./config', config)
 
     from eth2spec.phase0.spec import initialize_beacon_state_from_eth1
     from eth2spec.test.helpers.deposits import prepare_genesis_deposits
 
     import eth2fastspec as spec
+    import os
 
     print('preparing genesis deposits')
     deposits, deposit_root, _ = prepare_genesis_deposits(
@@ -41,6 +42,41 @@ def write_beacon_state():
     os.makedirs(os.path.dirname(BEACON_STATE_LOCATION), exist_ok=True)
     with open(BEACON_STATE_LOCATION, 'wb') as w:
         state.serialize(w)
+
+    return state
+
+
+def write_small_beacon_state():
+    from eth2spec.config.config_util import prepare_config
+    prepare_config('./config', 'minimal')
+
+    from eth2spec.phase0.spec import initialize_beacon_state_from_eth1
+    from eth2spec.test.helpers.deposits import prepare_genesis_deposits
+
+    import eth2fastspec as spec
+
+    import time
+
+    start = time.process_time()
+
+    print('preparing genesis deposits')
+    deposits, deposit_root, _ = prepare_genesis_deposits(
+        spec, spec.MIN_GENESIS_ACTIVE_VALIDATOR_COUNT, spec.MAX_EFFECTIVE_BALANCE, signed=True)
+
+    print(f'time taken: {time.process_time() - start}')
+
+    start = time.process_time()
+    print('initializing beacon state')
+    eth1_block_hash = b'\x12' * 32
+    eth1_timestamp = spec.MIN_GENESIS_TIME
+    state = initialize_beacon_state_from_eth1(eth1_block_hash, eth1_timestamp, deposits)
+
+    print(f'time taken: {time.process_time() - start}')
+
+    print('writing beacon state')
+    #os.makedirs(os.path.dirname(BEACON_STATE_LOCATION), exist_ok=True)
+    # with open(BEACON_STATE_LOCATION, 'wb') as w:
+    # state.serialize(w)
 
     return state
 
