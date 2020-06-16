d4889a5c087914a1eec94e8745d75b83d079b1bf
lsankar4033 <lsankar4033@gmail.com>
2020-06-16 09:50:24 -0700

    Increase default retry delay for `connect_rumor`

--- sclients/rumor.py
+++ sclients/rumor.py
@@ -2,7 +2,7 @@ from tenacity import retry, wait_fixed, stop_after_delay
 import trio
 
 
-@retry(sleep=trio.sleep, wait=wait_fixed(1), stop=stop_after_delay(10))
+@retry(sleep=trio.sleep, wait=wait_fixed(1), stop=stop_after_delay(15))
 async def connect_rumor(rumor, enr):
     await rumor.host.start()
     peer_id = await rumor.peer.connect(enr).peer_id()
